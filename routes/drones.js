fe482a5f3eaca84b21d79a5f20182d108566f77b
Joey Sutcliffe <joeysutcliffe0@gmail.comgit config --global user.email joeysutcliffe0@gmail.com>
2022-03-14 16:17:06 +0100

    part of i 3 done

--- routes/drones.js
+++ routes/drones.js
@@ -1,46 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
 // require the Drone model here
-const Drone = require('../models/Drone.model')
+const Drone = require("../models/Drone.model");
 
-router.get('/drones', async (req, res, next) => {
+router.get("/drones", async (req, res, next) => {
   // Iteration #2: List the drones
   try {
     const dronesFromDB = await Drone.find();
     console.log(dronesFromDB);
-    res.render("drones/list.hbs", { Drones : dronesFromDB });
-  }
-  catch (err) {
+    res.render("drones/list", { Drones: dronesFromDB });
+  } catch (err) {
     console.log("there was an error rendering to the hbs", err);
-  }
-  finally {
+  } finally {
     next();
-  } 
+  }
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get("/drones/create", (req, res, next) => {
+  res.render("drones/create-form");
+});
+
+router.post("/drones/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
 });
 
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
-
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post("/drones/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post("/drones/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
